bd3ed42adab4304c2f839dffb27eae4d5dc73d84
Jansora <zhangyue1936@gmail.com>
2020-08-14 22:37:11 +0800

    optimize deploy

--- conf/etcdserver/start-etcd-server.sh
+++ conf/etcdserver/start-etcd-server.sh
@@ -1,9 +1,8 @@
 mkdir -p $APP/data/logs/
 
-nohup /usr/bin/etcd  --config-file $APP/conf/etcdserver/server.yml >> $APP/data/logs/server.log &
-
-nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server1.yml >> $APP/data/logs/server1.log &
-nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server2.yml >> $APP/data/logs/server2.log &
-nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server3.yml >> $APP/data/logs/server3.log &
-nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server4.yml >> $APP/data/logs/server4.log &
-nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server5.yml >> $APP/data/logs/server5.log &
+nohup /usr/bin/etcd  --config-file $APP/conf/etcdserver/server.yml >>$APP/data/logs/server.log &
+nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server1.yml >>$APP/data/logs/server1.log &
+nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server2.yml >>$APP/data/logs/server2.log &
+nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server3.yml >>$APP/data/logs/server3.log &
+nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server4.yml >>$APP/data/logs/server4.log &
+nohup /usr/bin/etcd --config-file $APP/conf/etcdserver/server5.yml >>$APP/data/logs/server5.log &
