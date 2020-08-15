89d58863dc7a160b3c7d67b0e1bb62c5b4265087
Jansora <zhangyue1936@gmail.com>
2020-08-15 22:49:28 +0800

    optimize deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-sudo docker run -d -p 9090:9003 --name etcdui \
+sudo docker run -d -p 9090:9003 -p 1111:1111 -p 1112:2222 -p 1113:3333 -p 1114:4444 -p 1115:5555 --name etcdui --restart always \
 -v /root/docker/etcdui/data:/app/data \
 -v /root/g/etcdui/frontend/build:/app/dist jansora/etcdui:v1 \
